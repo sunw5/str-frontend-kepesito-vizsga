06a3bba847c035513e383647a264d0533bf1affd
Gabor Szasz <skywallalp@gmail.com>
2022-03-20 21:16:35 +0100

    *

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,18 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+  const nav = document.querySelector('nav')
+  const links = document.querySelectorAll('#link-list a');
+  const linksFiltered = [...links].filter(link => link.innerHTML.includes('outer-link'));
+  // console.log(linksFiltered);
 
+  for(const link of linksFiltered) {
+    link.setAttribute('target', '_blank');
+    link.innerHTML = `<strong>${link.innerHTML}</strong>`
+  }
 
+  nav.setAttribute('style', 'display:flex; flex-direction:column; width:30%; margin:0 auto; border:1px solid blue; padding:16px;');
+  // nav.setAttribute('style', 'flex-direction:column');
+
+}
+
+changeOuterLinks();
 export { changeOuterLinks };
