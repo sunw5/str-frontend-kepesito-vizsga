4aff5c388dbde26ab408614841e87eade1ce03bb
Gabor Szasz <skywallalp@gmail.com>
2022-03-20 19:33:19 +0100

    *

--- JS-task/task01-algorithm/main.js
+++ JS-task/task01-algorithm/main.js
@@ -1,15 +1,46 @@
-const movieList = [
-    { title: 'Vissza a jövőbe', year: 1990, timeInMinute: 95 },
-    { title: 'Titanic', year: 1997, timeInMinute: 182 },
-    { title: 'Charlie angyalai', year: 2003, timeInMinute: 99 },
-    { title: 'Robotzsaru', year: 1997, timeInMinute: 101 },
-    { title: 'Hangtalanul', year: 2015, timeInMinute: 120 },
-    { title: 'Csillagok között', year: 2020, timeInMinute: 180 },
-    { title: 'Top Gun', year: 1990, timeInMinute: 100 }
+const movieList = [{
+        title: 'Vissza a jövőbe',
+        year: 1990,
+        timeInMinute: 95
+    },
+    {
+        title: 'Titanic',
+        year: 1997,
+        timeInMinute: 182
+    },
+    {
+        title: 'Charlie angyalai',
+        year: 2003,
+        timeInMinute: 99
+    },
+    {
+        title: 'Robotzsaru',
+        year: 1997,
+        timeInMinute: 101
+    },
+    {
+        title: 'Hangtalanul',
+        year: 2015,
+        timeInMinute: 120
+    },
+    {
+        title: 'Csillagok között',
+        year: 2020,
+        timeInMinute: 180
+    },
+    {
+        title: 'Top Gun',
+        year: 1990,
+        timeInMinute: 100
+    }
 ]
 
 function movieFilter(list, year, maxTimeInMinutes) {
-
+    /* a megjelenés éve (adott évben megjelent)
+    időtartam szerint (maximális időtartam percben, kevesebb is lehet) */
+    return list.filter(movie => movie.year === year && movie.timeInMinute <= maxTimeInMinutes).map(movie => movie.name)
 }
 
-export { movieFilter };
+export {
+    movieFilter
+};
